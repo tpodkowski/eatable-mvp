051a9689263c71a1a17d68cda9e661cb7e7091e4
Tomasz Podkowski <tomasz@podkow.ski>
2019-04-02 20:30:27 +0200

    Allow adding and displaying products.

--- app/__init__.py
+++ app/__init__.py
@@ -6,18 +6,16 @@ db = SQLAlchemy()
 
 
 def create_app():
-    """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_ECHO"] = True
-#     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
     db.init_app(app)
     app.config.from_object('config.Config')
 
     with app.app_context():
-        # Imports
         from . import routes
-        # Initialize Global db
+        
         db.create_all()
 
         return app
--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,25 @@
-from flask import request, render_template
+from flask import request, render_template, redirect
 from flask import current_app as app, jsonify
 from .models import db, Product
 
 
 @app.route('/', methods=['GET'])
 def entry():
-  products = list(Product.query.all())
-  return render_template("products.html", products=products, title="Show Products")
-
-
-@app.route('/products', methods=['GET'])
-def get_products():
-  products = list(Product.query.all())
+  products = Product.query.all()
   return render_template("products.html", products=products, title="Show Products")
 
 @app.route('/products', methods=['POST'])
 def add_product():
-  form = request.form
-  print('DUPA')
   product = Product(
-    name=form['name'],
-    calories=form['calories'],
-    protein=form['protein'],
-    carbohydrates=form['carbohydrates'],
-    fat=form['fat']
+    name=request.form['name'],
+    calories=request.form['calories'],
+    protein=request.form['protein'],
+    carbohydrates=request.form['carbohydrates'],
+    fat=request.form['fat']
   )
   db.session.add(product)
   db.session.commit()
   
   products = Product.query.all()
-  return render_template("products.html", products=products, title="Show Products")
+  return redirect("/")
+  
