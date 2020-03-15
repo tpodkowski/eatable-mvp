5f3c99f4e3b9e2b29ae2bb10eb75466057ab1294
Tomasz Podkowski <tomasz@podkow.ski>
2020-03-15 18:25:29 +0100

    Add menu composer.

--- app/models.py
+++ app/models.py
@@ -1,27 +1,29 @@
 from . import db
 from sqlalchemy.dialects import postgresql
 
-products = db.Table('products',
-  db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
-  db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+recipes_to_products_table = db.Table('recipes_to_products',
+  db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
+  db.Column('product_id', db.Integer, db.ForeignKey('product.id'))
 )
 
 class Recipe(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(), index=True, unique=False, nullable=False)
-  description = db.Column(postgresql.JSON, index=False, unique=False, nullable=False)
-  image_url = db.Column(db.String(), index=False, unique=False, nullable=False)
-  url = db.Column(db.String(), index=False, unique=False, nullable=False)
-  products = db.relationship('Product', secondary=products, lazy='subquery', backref=db.backref('recipes', lazy=True))
-  def __repr__(self):
-    return repr(self.name)
+    __table_args__ = {'extend_existing': True} 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    description = db.Column(postgresql.JSON)
+    image_url = db.Column(db.String())
+    url = db.Column(db.String())
+    products = db.relationship('Product', secondary=recipes_to_products_table, back_populates="recipes")
+    def __repr__(self):
+            return repr(self.name)
 
 class Product(db.Model): 
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80), index=True, unique=False, nullable=False)
-  calories = db.Column(db.Integer, nullable=False)
-  protein = db.Column(db.Float, nullable=False)
-  carbohydrates	= db.Column(db.Float, nullable=False)
-  fat	= db.Column(db.Float, nullable=False)
+  name = db.Column(db.String(80))
+  calories = db.Column(db.Integer)
+  protein = db.Column(db.Float)
+  carbohydrates	= db.Column(db.Float)
+  fat	= db.Column(db.Float)
+  recipes = db.relationship('Recipe', secondary=recipes_to_products_table, back_populates="products") 
   def __repr__(self):
     return repr(self.name)
